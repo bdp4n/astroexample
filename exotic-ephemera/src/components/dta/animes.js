2b20cce8be2f2c46f9dbb9a14264cce49dcf26d8
bdp4n <josuegudu@gmail.com>
2023-07-21 00:40:20 -0600

    update of the proyect, cards list

---
+++ exotic-ephemera/src/components/dta/animes.js
@@ -0,0 +1,42 @@
+export const animes = [
+    {
+        id: "stains-gate",
+        title: "Stains Gate",
+        img: "https://cdn.myanimelist.net/images/anime/10/32023.jpg",
+    },
+    {
+        id: "Haikyuu",
+        title: "Haikyuu",
+        img: "https://cdn.myanimelist.net/images/anime/9/76662.jpg",
+    },
+    {
+        id: "Koe-No-Katachi",
+        title: "Koe no Katachi",
+        img: "https://cdn.myanimelist.net/images/anime/1122/96435.jpg",
+    },
+    {
+        id: "Sangatsu-No-Lion",
+        title: "Sangatsu no lion",
+        img: "https://cdn.myanimelist.net/images/anime/3/88469.jpg",
+    },
+    {
+        id: "Code-Geas",
+        title: "Code Geas",
+        img: "https://cdn.myanimelist.net/images/anime/1088/135089.jpg",
+    },
+    {
+        id: "Monster",
+        title: "Monster",
+        img: "https://cdn.myanimelist.net/images/anime/10/18793.jpg",
+    },
+    {
+        id: "Mob-Psycho-100",
+        title: "Mob Psycho 100",
+        img: "https://cdn.myanimelist.net/images/anime/1918/96303.jpg",
+    },
+    {
+        id: "Shingeki-No-Kyojin",
+        title: "Shingeki no Kyojin",
+        img: "https://cdn.myanimelist.net/images/anime/1948/120625.jpg",
+    },
+];
